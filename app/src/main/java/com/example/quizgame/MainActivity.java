c7a8d0fe88801c25b5254156be10a3f267b0cab4
DavidMakaryan <144596763+DavidMakaryan@users.noreply.github.com>
2024-03-29 14:45:10 +0400

    finished project

---
+++ app/src/main/java/com/example/quizgame/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.quizgame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String selectedTopic = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        final LinearLayout football = findViewById(R.id.footballLayout);
+        final LinearLayout countries = findViewById(R.id.countriesLayout);
+        final LinearLayout maths = findViewById(R.id.mathsLayout);
+        final LinearLayout facts = findViewById(R.id.factsLayout);
+
+        final Button startQuizBtn = findViewById(R.id.startQuizBtn);
+
+        football.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                selectedTopic = "football";
+                football.setBackgroundResource(R.drawable.round_back_white_stroke10);
+
+                countries.setBackgroundResource(R.drawable.round_back_white10);
+                maths.setBackgroundResource(R.drawable.round_back_white10);
+                facts.setBackgroundResource(R.drawable.round_back_white10);
+
+            }
+        });
+
+        countries.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                selectedTopic = "countries";
+                countries.setBackgroundResource(R.drawable.round_back_white_stroke10);
+
+                football.setBackgroundResource(R.drawable.round_back_white10);
+                maths.setBackgroundResource(R.drawable.round_back_white10);
+                facts.setBackgroundResource(R.drawable.round_back_white10);
+            }
+        });
+
+        maths.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                selectedTopic = "maths";
+                maths.setBackgroundResource(R.drawable.round_back_white_stroke10);
+
+                football.setBackgroundResource(R.drawable.round_back_white10);
+                countries.setBackgroundResource(R.drawable.round_back_white10);
+                facts.setBackgroundResource(R.drawable.round_back_white10);
+            }
+        });
+
+        facts.setOnClickListener(new View.OnClickListener()     {
+            @Override
+            public void onClick(View v) {
+
+                selectedTopic = "facts";
+                facts.setBackgroundResource(R.drawable.round_back_white_stroke10);
+
+                football.setBackgroundResource(R.drawable.round_back_white10);
+                countries.setBackgroundResource(R.drawable.round_back_white10);
+                maths.setBackgroundResource(R.drawable.round_back_white10);
+            }
+        });
+
+        startQuizBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (selectedTopic.isEmpty()) {
+
+                    Toast.makeText(MainActivity.this, "Выберите Викторину", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+                    intent.putExtra("selectedTopic", selectedTopic);
+                    startActivity(intent);
+                    finish();
+
+                }
+
+            }
+        });
+    }
+}
