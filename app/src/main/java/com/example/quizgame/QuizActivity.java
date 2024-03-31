d02a425ce363ce55ed58a8bf1041b4736ba9b6f4
DavidMakaryan <144596763+DavidMakaryan@users.noreply.github.com>
2024-03-31 15:02:36 +0400

    start the quiz

--- app/src/main/java/com/example/quizgame/QuizActivity.java
+++ app/src/main/java/com/example/quizgame/QuizActivity.java
@@ -1,8 +1,10 @@
 package com.example.quizgame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +12,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.appcompat.widget.AppCompatTextView;
+
+
 public class QuizActivity extends AppCompatActivity {
+
+    private TextView questions;
+    private TextView question;
+
+    private AppCompatButton option1, option2, option3, option4;
+    private AppCompatButton nextBtn;
+
+    private Timer quizTimer;
+
+    private int seconds = 0;
+    private int totalTimeInMins = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +49,45 @@ public class QuizActivity extends AppCompatActivity {
         final TextView timer = findViewById(R.id.timer);
         final TextView selectedTopicName = findViewById(R.id.selectedTopicName);
 
+        questions = findViewById(R.id.questions);
+        question = findViewById(R.id.question);
+
+        option1 = findViewById(R.id.option1);
+        option2 = findViewById(R.id.option2);
+        option3 = findViewById(R.id.option3);
+        option4 = findViewById(R.id.option4);
+
+        nextBtn = findViewById(R.id.nextBtn);
+
+
         final String getSelectedTopic = getIntent().getStringExtra("selectedTopic");
 
         selectedTopicName.setText(getSelectedTopic);
 
+
+
+    }
+
+    private void startTimer (TextView timerTextView) {
+
+        quizTimer = new Timer();
+        quizTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+               if( seconds == 0) {
+                   totalTimeInMins--;
+                   seconds = 59;
+               } else if (seconds == 0 && totalTimeInMins == 0) {
+                 quizTimer.purge();
+                 quizTimer.cancel();
+
+                   Toast.makeText(QuizActivity.this, "Время Вышло", Toast.LENGTH_SHORT).show();
+
+                   Intent intent = new Intent();
+                }
+
+            }
+        },1000,1000);
 
 
     }
